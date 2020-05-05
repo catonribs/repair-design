dc87aab2b964d3c14ce52637bec8f8f3a1d77577
Vlad <drfgt@yandex.ru>
2020-05-05 14:01:30 +0300

    fix browser-sync path

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,32 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const cssmin = require("gulp-cssmin");
+const rename = require("gulp-rename");
 
 // Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    gulp.watch("./*.html").on('change', browserSync.reload);
+gulp.task("browser-sync", function () {
+  browserSync.init({
+    server: {
+      baseDir: "src"
+    }
+  });
+  gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
-gulp.task('css', function (done) {
-    gulp.src('./src/css/*.css')
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist/css/'));
-    done();gulp-cssmin
+gulp.task("css", function (done) {
+  gulp
+    .src("./src/css/*.css")
+    .pipe(cssmin())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("./dist/css/"));
+  done();
+  gulp - cssmin;
 });
 
 function defaultTask(cb) {
-  console.log("Hello, world")
+  console.log("Hello, world");
   // place code for your default task here
   cb();
 }
 
-exports.default = defaultTask
+exports.default = defaultTask;

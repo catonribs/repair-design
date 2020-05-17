bddd6a4dfe8b4f5071c6f72fb8463626360cce6c
Vlad <drfgt@yandex.ru>
2020-05-17 16:25:44 +0300

    refact: try to do section steps 17.05.20 16-25

--- src/js/main.js
+++ src/js/main.js
@@ -74,13 +74,33 @@ $(document).ready(function () {
     },
 
   });
+
   var next = $('.swiper-button-next');
   var prev = $('.swiper-button-prev');
   var bullets = $('.swiper-pagination');
 
   next.css('left', prev.width() + 25 + bullets.width() + 27)
   bullets.css('left', prev.width() + 25)
+  
+  var mySwiper1 = new Swiper ('.swiper1', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination1',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
+    },
 
+  });
 
+  var next1 = $('.swiper-button-next1');
+  var prev1 = $('.swiper-button-prev1');
+  var bullets1 = $('.swiper-pagination1');
+
+  next1.css('left', prev1.width() + 25 + bullets1.width() + 27)
+  bullets1.css('left', prev1.width() + 25)
+
+  
 });
-
